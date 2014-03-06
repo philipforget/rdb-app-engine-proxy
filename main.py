fff946603cd63e6440e76e65a84306e5e811de44
Philip Forget <philipforget@gmail.com>
2014-03-06 02:27:10 -0500

    dont track lib, install it yourself

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-"""`main` is the top level module for your Flask application."""
+import requests
 
-# Import the Flask Framework
-from flask import Flask
+from flask import Flask, request, Response
+from google.appengine.api import memcache
+from hashlib import sha1
+
 app = Flask(__name__)
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
+
+RDB_TOKEN = '3317a37bc907d8541e0d3f71aef50a70bb8a83a8'
+# Number of requests an ip address can hit per minute, not a moving window
+MINUTE_LIMIT = 30
+# Dont put this in source control! KEEP IT HIDDEN FOREVER
+PARSER_URL = 'https://www.readability.com/api/content/v1/parser?token=%s&url={url}' % RDB_TOKEN
 
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+def proxy_rdb():
+    num_hits = memcache.get(request.remote_addr) or 0
+    if num_hits > MINUTE_LIMIT:
+        return 'Too many requests', 429
+
+    memcache.set(request.remote_addr, num_hits + 1, time=60)
+
+    url = request.args.get('url')
+    callback = request.args.get('callback')
+
+    if url is None:
+        return 'No `url` to parse', 400
+
+    url_hash = sha1(url).hexdigest()
+
+    content = memcache.get(url_hash)
+
+    if not content:
+        response = requests.get(PARSER_URL.format(url=url))
+        content = response.content
+        memcache.set(url_hash, content)
+
+    mimetype = 'application/json'
+    if callback:
+        content =  '{callback}({content})'.format(
+            callback=callback, content=content)
+        mimetype = 'application/javascript'
+
+    return Response(content, mimetype=mimetype, status=200)
 
 
 @app.errorhandler(404)
